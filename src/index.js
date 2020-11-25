f397b0e2772928b33623683c0a4a6b5db23a5167
antirex <55479324+antirex@users.noreply.github.com>
2020-11-25 10:34:58 +0530

    more ele

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>First react App</h1>, document.getElementById("root"));
+ReactDOM.render(
+<div>
+  <h1>First react App</h1>
+  <p>para graph</p>
+  <ul>
+    <li>Google</li>
+    <li>Apple</li>
+    <li>Facebook</li>
+  </ul>
+</div>, 
+document.getElementById("root")
+);
