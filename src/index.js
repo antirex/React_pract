178300c6c1554f60e74eb75191b500fd6b4ef3ad
antirex <55479324+antirex@users.noreply.github.com>
2020-11-25 10:48:40 +0530

    JSX + JS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Anshul";
+const surname = "Bamb";
+const num = "2";
+
 ReactDOM.render(
 <div>
-  <h1>First react App</h1>
-  <p>para graph</p>
-  <ul>
-    <li>Google</li>
-    <li>Apple</li>
-    <li>Facebook</li>
-  </ul>
+  <h1>First react App made by {name + " " +surname}</h1>
+  <h4>First react App made by {name} {surname}</h4>
+  <p>my lucky number is {num}</p>
+  {/* {values or expressions are permitted but not statements like if-else } */}
 </div>, 
 document.getElementById("root")
 );
