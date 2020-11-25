83941e3cf3b05e0ba3bcb8033bb04a813aae4a9d
antirex <55479324+antirex@users.noreply.github.com>
2020-11-25 12:54:08 +0530

    import_export_react

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -6,5 +6,18 @@ function Heading(){
       <h4>sub Heading</h4>
     </div>;
 }
+function add(x,y){
+  return (x+y);
+}
+function mul(x,y){
+  return (x*y);
+}
+function sub(x,y){
+  return (x-y);
+}
+function div(x,y){
+  return (x/y);
+}
 
 export default Heading;
+export {add, mul,sub, div};
--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {add,sub,mul,div} from "./Heading";
 function List(){
   return <ul>
-    <li>Google</li>
-    <li>Facebook</li>
-    <li>apple</li>
+    <li>{add(9,1)}</li>
+    <li>{sub(100,10)}</li>
+    <li>{mul(25,3)}</li>
+    <li>{div(100,10)}</li>
   </ul>
 }
 
