eba42f898c7c36ad34043db1aeb0f5bdf555020a
antirex <55479324+antirex@users.noreply.github.com>
2020-11-26 15:02:26 +0530

    props-into

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+function Card(props){
+  return (
+  <div>
+    <h1>My Contacts</h1>
+    <h2>{props.name}</h2>
+    <img
+      src={props.img_src}
+      alt="avatar_img"
+    />
+    <p>{props.phone}</p>
+    <p>{props.email}</p>
+  </div>
+  );
+}
+
+ReactDOM.render(<div>
+  <Card
+  name="Beyonce"
+  img_src ="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+  email ="b@beyonce.com"
+  phone="+123 456 789"
+  />
+  <Card
+  name="Jack Bauer"
+  img_src ="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+  email ="jack@nowhere.com"
+  phone="+987 654 32"
+  />
+</div>,document.getElementById("root"));
