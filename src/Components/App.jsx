c07045ec99baff046b8809786fe02c655b2bfe6c
antirex <55479324+antirex@users.noreply.github.com>
2020-11-30 11:18:00 +0530

    ternary operator bothVs

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import Login from "./Login";
 
 var isLoggedIn = false;
+const date = new Date().getHours();
 
-function renderConditionality() {
-  if (isLoggedIn) {
-    return <h1>Hello User!</h1>;
-  } else {
-    return <Login />;
-  }
-}
+// function renderConditionality() {
+//   if (isLoggedIn) {
+//     return <h1>Hello User!</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{renderConditionality()}</div>;
+  return (
+    <div className="container">
+      {isLoggedIn === true ? <h1>Hello User!</h1> : <Login />}
+      {date > 10 && <p>Boombaalaaa</p>}
+      {/* here we are using the react && where it checks if cond true then render p tag or else nothing  */}
+    </div>
+  );
 }
 
 export default App;
