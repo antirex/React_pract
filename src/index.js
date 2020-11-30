83afa625092f7fe6921ddd28e7229dcb13d2090f
antirex <55479324+antirex@users.noreply.github.com>
2020-11-30 11:02:33 +0530

    conditionalRendering_1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
-import emojipedia from "./emojipedia";
-// import App from "./App";
+import App from "../src/Components/App";
+// import emojipedia from "./emojipedia";
+// // import App from "./App";
 
-// ReactDOM.render(<App/>,document.getElementById("root"));
-const numbers = [10, 4, 56, 78, 23, 45, 6];
-
-const newNumbers = numbers.filter((num) => num > 20); //filter
-console.log(newNumbers);
-
-const add = numbers.reduce((accum, num) => {
-  console.log("Accum: " + accum);
-  console.log("Num: " + num);
-  return accum + num;
-}); //reduce
-console.log(add);
-
-//map/reduce/filter
-var desc = emojipedia.map((descrip) => descrip.meaning.substring(0, 100)); //map
-
-console.log(desc);
+ReactDOM.render(<App />, document.getElementById("root"));
